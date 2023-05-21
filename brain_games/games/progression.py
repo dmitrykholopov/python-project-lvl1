e3ede2ec27bddd11e42a65af87d2b6a80bbad614
dmitrykholopov <106934658+dmitrykholopov@users.noreply.github.com>
2023-05-21 15:43:27 +0300

    Update progression.py
    Reduce complexity of get_progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,26 +3,21 @@
 from random import randint
 
 #   progression data:
-MIN_LENGTH = 10
-MAX_LENGTH = 15
-MIN_FIRST_ELEMENT = 30
-MAX_FIRST_ELEMENT = 50
-MIN_STEP = 1
-MAX_STEP = 10
+MIN_MAX_LENGTH = 10, 15
+MIN_MAX_FIRST_ELEMENT = 30, 50
+MIN_MAX_STEP = 1, 10
 
 GAME_QUESTION = 'What number is missing in the progression?'
 
 
-def get_progression(min_length,
-                    max_length,
-                    min_first_element,
-                    max_first_element,
-                    min_step,
-                    max_step
-                    ):
-    progression_step = randint(min_step, max_step)
-    progression_first_elem = randint(min_first_element, max_first_element)
-    progression_elements_count = randint(min_length, max_length)
+def get_progression(min_max_length,
+                    min_max_first_element,
+                    min_max_step
+                   ):
+    progression_step = randint(min_max_step)
+    progression_first_elem = randint(min_max_first_element)
+    progression_elements_count = randint(min_max_length)
+
     return [i for i in range(
             progression_first_elem,
             progression_first_elem
@@ -33,18 +28,17 @@ def get_progression(min_length,
 
 
 def get_round_data():
-    generated_progression = get_progression(MIN_LENGTH,
-                                            MAX_LENGTH,
-                                            MIN_FIRST_ELEMENT,
-                                            MAX_FIRST_ELEMENT,
-                                            MIN_STEP,
-                                            MAX_STEP
-                                            )
+    generated_progression = get_progression(MIN_MAX_LENGTH,
+                                            MIN_MAX_FIRST_ELEMENT,
+                                            MIN_MAX_STEP)
+    
     hidden_element = randint(0, len(generated_progression) - 1)
     right_answer = str(generated_progression.pop(hidden_element))
     generated_progression.insert(hidden_element, '..')
     question = ''
+
     for element in generated_progression:
         question += str(element) + ' '
     question = question.rstrip()
+
     return question, right_answer
